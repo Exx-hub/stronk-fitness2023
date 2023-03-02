beb0c8a233e991608dcb33fc8e21a4895bef694d
Alvin Acosta <alvin.acosta@movon.com.ph>
2023-03-02 19:04:43 +0800

    add text if no wods yet

--- app/wod-list/page.tsx
+++ app/wod-list/page.tsx
@@ -12,7 +12,13 @@ async function WodList() {
     <section className="h-full">
       <div className="h-full bg-wodList bg-cover bg-center">
         <div className="w-[90%] md:w-[65%] max-w-2xl mx-auto pt-10 flex flex-col gap-1">
-          {wods && wods?.map((wod) => <Wod {...wod} key={wod.id} />)}
+          {wods?.length ? (
+            wods?.map((wod) => <Wod {...wod} key={wod.id} />)
+          ) : (
+            <h2 className="text-white text-sm lg:text-lg text-center">
+              -- Start Today. Add a Workout. --
+            </h2>
+          )}
           <hr className="my-1" />
           <CreateWod />
         </div>
