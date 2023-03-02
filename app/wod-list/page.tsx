e7ae08e9abcdbd60629ce0b10e931f83d4e986d7
Alvin Acosta <alvin.acosta@movon.com.ph>
2023-03-02 16:59:05 +0800

    fix bug where when signed out, fetches all wods instead of fetching nothing

--- app/wod-list/page.tsx
+++ app/wod-list/page.tsx
@@ -3,19 +3,16 @@ import { getUserWods } from "@/lib/prismaHelpers";
 import CreateWod from "./CreateWod";
 import Wod from "./Wod";
 
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-
 async function WodList() {
   const wods = await getUserWods();
 
-  // console.log("wods", wods);
+  console.log("wods", wods);
 
   return (
     <section className="h-full relative">
       <div className="h-full bg-wodList bg-cover bg-center">
         <div className="w-[90%] md:w-[65%] max-w-2xl mx-auto pt-10 flex flex-col gap-1">
-          {wods && wods.map((wod) => <Wod {...wod} key={wod.id} />)}
+          {wods && wods?.map((wod) => <Wod {...wod} key={wod.id} />)}
           <hr className="my-1" />
           <CreateWod />
         </div>
--- lib/prismaHelpers.ts
+++ lib/prismaHelpers.ts
@@ -40,9 +40,16 @@ export const getWodById = async (id: string) => {
 export const getUserWods = async () => {
   const session = await getServerSession(authOptions);
 
+  if (!session) {
+    return null;
+  }
+
   const userId = session?.user?.id;
 
-  const userWods = await client.wod.findMany({ where: { userId }, include: { exercises: true } });
+  const userWods = await client.wod.findMany({
+    where: { userId: userId },
+    include: { exercises: true },
+  });
 
   return userWods;
 };
